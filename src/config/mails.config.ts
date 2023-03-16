a300ce1ddee8470bf5caf4f1f41e33f106d6ee89
onxzy <contact@onxzy.dev>
2023-03-16 14:27:19 +0100

    SMTP Check on server init and option to disable mails

--- src/config/mails.config.ts
+++ src/config/mails.config.ts
@@ -1,7 +1,7 @@
 import { registerAs } from '@nestjs/config';
 
 export const mailsConfig = registerAs('mails', () => ({
-  host: process.env.SMTP_HOST || 'localhost',
+  host: process.env.SMTP_HOST || false,
   port: parseInt(process.env.SMTP_PORT || '587'),
   secure: Boolean(parseInt(process.env.SMTP_SECURE || '0')),
   auth: {
--- src/mails/mails.service.ts
+++ src/mails/mails.service.ts
@@ -8,11 +8,29 @@ export class MailsService {
   mailTransporter: Transporter;
 
   constructor(configService: ConfigService) {
-    this.mailTransporter = createTransport(configService.get('mails'));
+    const config = configService.get('mails');
+    if (config.host) {
+      this.mailTransporter = createTransport(configService.get('mails'));
+
+      // Check connection
+      this.mailTransporter.verify((err, _) => {
+        if (err) throw err
+        else console.info('[Mails Service] SMTP server online !');
+      })
+    } else {
+      this.mailTransporter = null;
+      console.warn('[Mails Service] No SMTP server configured !');
+    }
+    
   }
 
   async sendMail(to: string, msg: MailTemplateReady) {
-    const infos = await this.mailTransporter.sendMail({ to, ...msg });
-    return infos.messageId as boolean;
+    if (this.mailTransporter) {
+      const infos = await this.mailTransporter.sendMail({ to, ...msg });
+      return infos.messageId as boolean;
+    } else {
+      console.info(`[Mails Service] No SMTP server - Mail "${msg.subject.substring(0, 31)}" to <${to}> not sent`);
+      return true;
+    }
   }
 }
